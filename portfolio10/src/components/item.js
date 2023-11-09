a3faa9ae4322b4199697f68c05b419581162345c
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-11-09 08:52:51 -0300

    destacados

--- portfolio10/src/components/item.js
+++ portfolio10/src/components/item.js
@@ -15,34 +15,40 @@ export default function Item() {
       });
   }, []);
 
-  
+  const info = (idP) => {
+    let infoP = Personajes.find((personaje) => personaje.id === idP);
+    console.log(infoP);
+  };
+
   return (
     <div className="row">
-      {Personajes.map((personaje, index) => (
-        <div className="col-md-4 mb-5" key={index}>
-          <div
-            className="portfolio-item mx-auto"
-            data-bs-toggle="modal"
-            data-bs-target="#portfolioModal1"
-          >
-            <div className="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
-              <div className="portfolio-item-caption-content text-center text-white">
-                <i className="fas fa-plus fa-3x"></i>
-              </div>
+    {Personajes.map((personaje, index) => (
+      <div className="col-md-4 mb-5" key={index}>
+        <div className="portfolio-item mx-auto">
+            <div className="portfolio-item-caption-content text-center text-white">
+              <i className="fas fa-plus fa-3x"></i>
             </div>
-            <img
-              className="img-fluid"
-              src={personaje.imagen}
-              alt={personaje.titulo}
-              style={imgStyle} // Apply inline styles here
-            />
           </div>
+          <img
+            className="img-fluid"
+            src={personaje.imagen}
+            alt={personaje.titulo}
+            style={imgStyle}
+            onClick={() => info(personaje.id)}
+            onMouseOver={(e) => (e.target.style.filter = "brightness(120%)")} // Change brightness on hover
+            onMouseOut={(e) => (e.target.style.filter = "brightness(50%)")} // Reset brightness on mouse leave
+          />
         </div>
-      ))}
-    </div>
+    
+    ))}
+  </div>
   );
 }
+
 const imgStyle = {
-  height: "350px", // Set the fixed height for all images
-  objectFit: "cover", // Ensure the image covers the specified height without stretching
+  height: "400px",
+    objectFit: "cover",
+    transition: "filter 0.3s ease-in-out",
+    filter: "brightness(50%)", // Initial brightness
+    cursor: "pointer",
 };
